ab3300cc4949988f1ab10f5be2d51c0a084c911d
HungTH <tranhoaihung01@gmail.com>
2024-03-31 21:08:47 +0700

    <feat> add example

---
+++ example/eidExample/eidExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  eidExample
+//
+//  Created by H-Solutions on 31/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ example/eidExample/eidExample/eidExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  eidExampleApp.swift
+//  eidExample
+//
+//  Created by H-Solutions on 31/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct eidExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
