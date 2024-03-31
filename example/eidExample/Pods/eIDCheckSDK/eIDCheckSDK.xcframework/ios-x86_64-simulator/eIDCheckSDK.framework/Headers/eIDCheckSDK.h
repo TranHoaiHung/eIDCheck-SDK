f544841688b4f3bafdbadadcff6981fc23f0463e
HungTH <tranhoaihung01@gmail.com>
2024-03-31 21:23:48 +0700

    <feat> add pod file examples

---
+++ example/eidExample/Pods/eIDCheckSDK/eIDCheckSDK.xcframework/ios-x86_64-simulator/eIDCheckSDK.framework/Headers/eIDCheckSDK.h
@@ -0,0 +1,19 @@
+//
+//  eIDCheckSDK.h
+//  eIDCheckSDK
+//
+//  Created by H-Solutions on 29/11/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for eIDCheckSDK.
+FOUNDATION_EXPORT double eIDCheckSDKVersionNumber;
+
+//! Project version string for eIDCheckSDK.
+FOUNDATION_EXPORT const unsigned char eIDCheckSDKVersionString[];
+
+
+// In this header, you should import all the public headers of your framework using statements like #import <eIDCheckSDK/PublicHeader.h>
+
+
