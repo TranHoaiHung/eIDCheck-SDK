411709ff7c91b29946928988666fcdb6bb797e05
HungTH <tranhoaihung01@gmail.com>
2024-03-31 23:28:47 +0700

    commit file

--- example/eidExample/Pods/Target Support Files/Pods-eidExample/Pods-eidExample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_eidExample : NSObject
-@end
-@implementation PodsDummy_Pods_eidExample
-@end
--- example/eidExample/Pods/Target Support Files/QKMRZScanner/QKMRZScanner-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_QKMRZScanner : NSObject
-@end
-@implementation PodsDummy_QKMRZScanner
-@end
